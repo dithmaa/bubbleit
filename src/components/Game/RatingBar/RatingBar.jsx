d8131ea41d3d6eb458a36eacc6e424c39e84dc9e
Baltukov Nomto <nomto_04@mail.ru>
2024-04-11 00:09:14 +0800

    second commit

--- src/components/Game/RatingBar/RatingBar.jsx
+++ src/components/Game/RatingBar/RatingBar.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./RatingBar.module.css";
+import axios from "axios";
+import goldCup from "../../../assets/img/gold.png";
 
 function RatingBar({ onClick }) {
+  const [firstUser, setFirstUser] = useState([
+    { clickAmount: 10048199, id: 1, name: "Name" },
+  ]);
+  // console.log(firstUser[0]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://65eafaa243ce16418932f611.mockapi.io/popit/popit?sortBy=clickAmount&order=desc&limit=1&page=1"
+      )
+      .then(({ data }) => {
+        setFirstUser(data);
+      });
+  }, []);
   return (
     <div onClick={onClick} className={styles.root}>
-      Очки остальных игроков
+      <img style={{ width: "20px" }} src={goldCup} alt="gold cup" />
+      <span
+        style={{
+          paddingLeft: "20px",
+          fontSize: "18px",
+          color: "#ce7aff",
+          fontWeight: "bold",
+        }}
+      >
+        {firstUser[0].clickAmount}
+      </span>
+      <span style={{ fontSize: "14px", paddingLeft: "10px" }}>
+        {firstUser[0].name}
+      </span>
     </div>
   );
 }
