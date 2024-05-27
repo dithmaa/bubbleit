c7e06d6a141503cd9225f9885fce5ba83e059f82
Baltukov Nomto <nomto_04@mail.ru>
2024-05-27 14:00:21 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -67,9 +67,9 @@ function App() {
     <div className="App">
       <div style={{ color: "#fff" }}>
         link:{" "}
-        {tg.initDataUnsafe?.user?.photo_url == null
-          ? "пусто"
-          : tg.initDataUnsafe?.user?.photo_url}{" "}
+        {tg.initDataUnsafe?.user?.photo == null
+          ? "없음"
+          : tg.initDataUnsafe?.user?.photo}{" "}
         <br /> <hr />
         id: {tg.initDataUnsafe?.user?.id} <br /> <hr />
         firstname: {tg.initDataUnsafe?.user?.first_name} <br /> <hr />
