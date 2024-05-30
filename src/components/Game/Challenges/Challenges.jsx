3c77ceecb1e6dabf61a1cb409fec3c929aa79ece
Baltukov Nomto <nomto_04@mail.ru>
2024-05-31 01:24:01 +0800

    init

--- src/components/Game/Challenges/Challenges.jsx
+++ src/components/Game/Challenges/Challenges.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import styles from "./Challanges.module.scss";
 import avatar from "../../../assets/img/avatar1.jpg";
+import avatar2 from "../../../assets/img/avatar2.jpg";
 import loadGif from "../../../assets/img/loading.gif";
 import { useState } from "react";
 import axios from "axios";
 
 export default function Challenges({ closePresent, currentID }) {
   const [isShowBtnLoad, setShowBtnLoad] = useState(0);
+  const [isShowBtnLoad2, setShowBtnLoad2] = useState(0);
   const [isCompleted, setCompleted] = useState(0);
+  const [isCompleted2, setCompleted2] = useState(0);
   const [alreadyCompleted, setAlreadyCompleted] = useState(0);
+  const [alreadyCompleted2, setAlreadyCompleted2] = useState(0);
   const [clickAmountUser, setClickAmountUser] = useState(0);
   useEffect(() => {
     axios
@@ -17,10 +21,11 @@ export default function Challenges({ closePresent, currentID }) {
       )
       .then(({ data }) => {
         setAlreadyCompleted(data.isCompletedMission);
+        setAlreadyCompleted2(data.isCompletedMiss);
         setClickAmountUser(data.clickAmount);
       });
   }, []);
-  console.log("alreadyCompleted", alreadyCompleted);
+  console.log("alreadyCompleted2", alreadyCompleted2);
   const handleCheckSub = (e) => {
     e.preventDefault();
     window.open("https://t.me/cryptopoonn");
@@ -40,6 +45,30 @@ export default function Challenges({ closePresent, currentID }) {
     setTimeout(() => {
       setCompleted(1);
       setShowBtnLoad(0);
+      setTimeout(() => {
+        window.location.reload();
+      }, 1200);
+    }, 20000);
+  };
+  const handleCheckSub2 = (e) => {
+    e.preventDefault();
+    window.open("https://t.me/bubbleit_group");
+    setTimeout(() => {
+      setShowBtnLoad2(1);
+    }, 500);
+    const newClickAmount = clickAmountUser + 1000000;
+    setTimeout(() => {
+      axios.put(
+        `https://65eafaa243ce16418932f611.mockapi.io/popit/popit/${currentID}`,
+        {
+          isCompletedMiss: true,
+          clickAmount: newClickAmount,
+        }
+      );
+    }, 17000);
+    setTimeout(() => {
+      setCompleted2(1);
+      setShowBtnLoad2(0);
       setTimeout(() => {
         window.location.reload();
       }, 1200);
@@ -75,6 +104,33 @@ export default function Challenges({ closePresent, currentID }) {
                   <img className={styles.getBtnLoad} src={loadGif} alt="load" />
                 ) : (
                   <span>{isCompleted ? "Выполнено" : "Подписаться"}</span>
+                )}
+              </button>
+            ) : (
+              <button className={styles.getBtn} disabled={true}>
+                <span>Выполнено</span>
+              </button>
+            )}
+          </div>
+          <div className={styles.present__item}>
+            <div className="present__avatar">
+              <img className={styles.avatar} src={avatar2} alt="" />
+            </div>
+            <div className={styles.present__title}>
+              <span>Bubble IT</span>
+              <span>Награда: 1 млн пузырей</span>
+            </div>
+            {!alreadyCompleted2 ? (
+              <button
+                disabled={isCompleted2}
+                onClick={handleCheckSub2}
+                href="#"
+                className={styles.getBtn}
+              >
+                {isShowBtnLoad2 ? (
+                  <img className={styles.getBtnLoad} src={loadGif} alt="load" />
+                ) : (
+                  <span>{isCompleted2 ? "Выполнено" : "Подписаться"}</span>
                 )}
               </button>
             ) : (
